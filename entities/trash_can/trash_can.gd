db66fe9f880cc7956e2180d08e4507becb01f416
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-07-02 14:13:54 +0000

    Moved code to hide and show trash can into level_01 script. I did this so that i could reuse the trash can scene wihout having to worry about buying enough items for the trash can to be visible.

--- entities/trash_can/trash_can.gd
+++ entities/trash_can/trash_can.gd
@@ -1,40 +1,8 @@
 extends Node3D
 
 
-## How many items the player needs to buy before the trash can can become visible
-@export_range(1, 99) var become_visible_buy_count: int = 5
-@export_range(1, 99) var trash_capacity: int = 5
-@export var mesh: Node3D
-
+@export_range(1, 99) var trash_capacity: int = 15
 var items_trashed: int = 0
-var buy_count: int = 0
-
-
-func _ready() -> void:
-	# Hide and disable mesh when the game starts
-	_disable_mesh()
-	# Connect event from vending machine
-	EventBus.vending_machine_code_submitted.connect(_on_vending_machine_code_submitted)
-
-
-func _enable_mesh() -> void:
-	mesh.show()
-	mesh.process_mode = Node.PROCESS_MODE_INHERIT
-
-
-func _disable_mesh() -> void:
-	mesh.hide()
-	mesh.process_mode = Node.PROCESS_MODE_DISABLED
-
-
-func _on_vending_machine_code_submitted(_code: String, is_valid: bool) -> void:
-	if is_valid:
-		buy_count += 1
-
-
-func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
-	if not mesh.visible and buy_count >= become_visible_buy_count:
-		_enable_mesh()
 
 
 func _on_destroy_area_body_entered(body: Node3D) -> void:
--- levels/level_01/level_01.gd
+++ levels/level_01/level_01.gd
@@ -2,14 +2,20 @@ extends Node3D
 
 
 @export var train_station_tunnel_spawn_point: Marker3D
+var buy_count: int = 0
 
 
 func _ready() -> void:
+	# Hide and disable trash can when the game starts
+	_disable_trash_can()
 	# Connect signals
 	EventBus.vending_machine_code_submitted.connect(_on_vending_machine_code_submitted)
 
 
-func _on_vending_machine_code_submitted(code: String, _valid: bool) -> void:
+func _on_vending_machine_code_submitted(code: String, valid: bool) -> void:
+	if valid:
+		buy_count += 1
+	
 	match code:
 		"1996":
 			# Spawn train station tunnel
@@ -17,3 +23,24 @@ func _on_vending_machine_code_submitted(code: String, _valid: bool) -> void:
 			var spawn_transform: Transform3D = train_station_tunnel_spawn_point.global_transform
 			EventBus.load_level.emit(scene_path, spawn_transform)
 			$Ground/CSGBox3D.show()
+
+
+@export_group("Trash can")
+## How many items the player needs to buy before the trash can can become visible
+@export_range(1, 99) var trash_can_become_visible_buy_count: int = 5
+@export var trash_can: Node3D
+
+
+func _on_trash_can_visible_on_screen_notifier_screen_exited() -> void:
+	if not trash_can.visible and buy_count >= trash_can_become_visible_buy_count:
+		_enable_trash_can()
+
+
+func _enable_trash_can() -> void:
+	trash_can.show()
+	trash_can.process_mode = Node.PROCESS_MODE_INHERIT
+
+
+func _disable_trash_can() -> void:
+	trash_can.hide()
+	trash_can.process_mode = Node.PROCESS_MODE_DISABLED
