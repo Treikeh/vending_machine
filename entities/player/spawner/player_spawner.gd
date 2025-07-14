2fe77d4ae38cc57b75d0b18bd46eca44b326fce2
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-07-14 07:37:26 +0000

    Used the new spawn callback to emit a signal when the player is spawned

--- entities/player/spawner/player_spawner.gd
+++ entities/player/spawner/player_spawner.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 
+signal player_spawned
+
+
 const PLAYER_SCENE: String = "uid://b62jt4e6safbv"
 const HUD_SCENE: String = "uid://codyyu2jnkho5"
 
@@ -13,5 +16,9 @@ func _ready() -> void:
 	
 	# Spawn player if there is no player in the scene
 	if get_tree().get_first_node_in_group("player") == null:
-		Globals.main.load_level(PLAYER_SCENE, global_transform)
+		Globals.main.load_level(PLAYER_SCENE, global_transform, _player_spawn_callback)
 		Globals.main.load_menu(HUD_SCENE)
+
+
+func _player_spawn_callback() -> void:
+	player_spawned.emit()
