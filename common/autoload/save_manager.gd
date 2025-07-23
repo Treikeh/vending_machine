4eea1bcfc3586be6f25a3f4ab42404b8b0d11928
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-07-23 17:08:17 +0000

    Made the code on the save manager smaller.

--- common/autoload/save_manager.gd
+++ common/autoload/save_manager.gd
@@ -1,5 +1,4 @@
 extends Node
-#TODO: Encode save data to hex or something (Not necessary, but might be a good idea)
 #TODO: Find a way to save items that have been spawned into levels
 
 # Saving/Loading level data to/from file
@@ -7,6 +6,7 @@ const USER_PATH: String = "user://savegame.save"
 const DEBUG_PATH: String = "res://debug/savegame.ini"
 
 
+var _file_access: FileAccess
 var _save_data: Dictionary
 
 @onready var _save_path: String = DEBUG_PATH if OS.is_debug_build() else USER_PATH
@@ -15,12 +15,8 @@ var _save_data: Dictionary
 func _ready() -> void:
 	# Load data when game starts
 	_load_data_from_file()
-	#tree_exiting.connect(_on_tree_exiting)
-
-
-func _on_tree_exiting() -> void:
 	# Save data when exiting the game
-	_save_data_to_file()
+	#tree_exiting.connect(_save_data_to_file)
 
 
 func add_save_data(key:String, data: Dictionary) -> void:
@@ -33,25 +29,17 @@ func get_save_data(key: String) -> Dictionary:
 
 func _save_data_to_file() -> void:
 	# Create/open a file to write to
-	var save_file := FileAccess.open(_save_path, FileAccess.WRITE)
-	# Turn save data into a string
-	var data_string: String = JSON.stringify(_save_data, "\t")
-	# Save data to file
-	save_file.store_string(data_string)
+	_file_access = FileAccess.open(_save_path, FileAccess.WRITE)
+	# Turn _save_data dict into a string and save it on the save file
+	_file_access.store_string(JSON.stringify(_save_data, "\t"))
+	_file_access.close()
 
 
 func _load_data_from_file() -> void:
 	# Check if save file exists
-	if not FileAccess.file_exists(_save_path):
-		return
-	
-	# Turn text from save file into a dictionary using JSON
-	var save_file := FileAccess.open(_save_path, FileAccess.READ)
-	var json := JSON.new()
-	var parse_result: Error = json.parse(save_file.get_as_text())
-	if not parse_result == OK:
-		print("JSON Parse Error: %s, at line %s" % json.get_error_message(), json.get_error_line())
-		return
-	
-	# Set save data
-	_save_data = json.data
+	if FileAccess.file_exists(_save_path):
+		# Open save file so that data can be read from it
+		_file_access = FileAccess.open(_save_path, FileAccess.READ)
+		# Parse the save file and set the _save_dict to the result
+		_save_data = JSON.parse_string(_file_access.get_as_text())
+		_file_access.close()
