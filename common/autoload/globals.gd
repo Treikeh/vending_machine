70fb2990c57835a69b6eb536bb4d4c83891533ad
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-08-04 15:32:30 +0000

    Changed name of quit level to quit scene and move it to the gui folder.

--- common/autoload/globals.gd
+++ common/autoload/globals.gd
@@ -5,7 +5,7 @@ extends Node
 # Paths I have saved because i'm too lazy to find them in the file system
 const WORLD_ENV_PATH: String = "uid://4jbcov8xso67"
 const MAIN_MENU_PATH: String = "uid://wwxe07fon8hy"
-const QUIT_LEVEL_PATH: String = "uid://cd8dmofo47nsb"
+const QUIT_SCENE_PATH: String = "uid://cd8dmofo47nsb"
 const SETTINGS_MENU_PATH: String = "uid://t0lpsh2ot3se"
 const CREDITS_PATH: String = "uid://dkyrn54813ilp"
 
--- gui/pause_menu/pause_menu.gd
+++ gui/pause_menu/pause_menu.gd
@@ -37,5 +37,5 @@ func _on_main_menu_button_pressed() -> void:
 func _on_quit_button_pressed() -> void:
 	get_tree().paused = false
 	SaveManager.add_save_data("active_levels", LevelManager.get_active_levels())
-	LevelManager.load_level(Globals.QUIT_LEVEL_PATH)
+	LevelManager.load_level(Globals.QUIT_SCENE_PATH)
 	GuiManager.unload_all_menus()
