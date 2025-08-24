3f481bcd30f62445b1c46a6c06897c7fb3d39c41
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-08-24 18:53:19 +0200

    Added the locations board to level 01 and started working on the elevator to hell.

--- levels/level_01/level_01.gd
+++ levels/level_01/level_01.gd
@@ -7,17 +7,38 @@ var _buy_count: int = 0
 func _on_vending_machine_code_submitted(code: String, valid: bool) -> void:
 	if valid:
 		_buy_count += 1
+		# Allow trash can to become visible when buying enough stuff
+		if _buy_count >= _trash_can_become_visible_buy_count:
+			_trash_can_can_become_visible = true
+			if not _trash_can_screen_notifier.is_on_screen():
+				_set_node_active(_trash_can, true)
 	
-	# Allow the train station tunnel to spawn when typing in the right code
-	if code == "1996":
-		_tunnel_state = Tunnel_State.CAN_SPAWN
-	
-	if code == "666":
-		# Spawn elevator
-		pass
-	
-	if code == "41":
-		_basketball_bought = true
+	# Do stuff when typing in the right codes
+	match code:
+		"41": # Basketball
+			_basketball_bought = true
+			if not _basketball_hoop_screen_notifier.is_on_screen():
+				_set_node_active(_basketball_hoop, true)
+				
+		"666": # Hell elevator
+			_show_hellevator()
+		"1996": # Train station tunnel
+			_tunnel_state = Tunnel_State.CAN_SPAWN
+			if not _tunnel_path_screen_notifier.is_on_screen():
+				_spawn_train_station_tunnel()
+
+
+func _set_node_active(
+		node: Node3D,
+		active: bool,
+		active_process_mode: Node.ProcessMode = Node.PROCESS_MODE_INHERIT
+) -> void:
+	if active:
+		node.show()
+		node.process_mode = active_process_mode
+	else:
+		node.hide()
+		node.process_mode = Node.PROCESS_MODE_DISABLED
 
 
 #region Train station tunnel
@@ -27,6 +48,7 @@ enum Tunnel_State {HIDDEN, CAN_SPAWN, SPAWNED}
 @export_group("Train station tunnel")
 @export var _train_station_tunnel_spawn_point: Marker3D
 @export var _tunnel_path: Node3D
+@export var _tunnel_path_screen_notifier: VisibleOnScreenNotifier3D
 
 var _tunnel_state: Tunnel_State = Tunnel_State.HIDDEN: set = _set_tunnel_state
 
@@ -53,26 +75,48 @@ func _spawn_train_station_tunnel() -> void:
 
 
 
+#region Hell
+
+const HELLEVATOR_MOVE_TIME: float = 5.0
+
+@export_group("Hell")
+@export var _hellevator: Node3D
+
+
+func _show_hellevator() -> void:
+	_hellevator.position.y = -4.0
+	_set_node_active(_hellevator, true)
+	
+	var move_tween: Tween = create_tween()
+	move_tween.tween_property(_hellevator, "position:y", 0.0, HELLEVATOR_MOVE_TIME)
+
+
+func _set_hellevator_active(active: bool) -> void:
+	if active:
+		_hellevator.show()
+		_hellevator.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		_hellevator.hide()
+		_hellevator.process_mode = Node.PROCESS_MODE_DISABLED
+
+#endregion
+
+
+
 #region Trash can
 
 @export_group("Trash can")
 ## How many items the player needs to buy before the trash can can become visible
 @export_range(1, 99) var _trash_can_become_visible_buy_count: int = 5
+@export var _trash_can_screen_notifier: VisibleOnScreenNotifier3D
 @export var _trash_can: StaticBody3D
+
+var _trash_can_can_become_visible: bool = false
 
 
 func _on_trash_can_screen_notifier_screen_exited() -> void:
-	if _buy_count >= _trash_can_become_visible_buy_count and not _trash_can.visible:
-		_set_trash_can_state(true)
-
-
-func _set_trash_can_state(active: bool) -> void:
-	if active:
-		_trash_can.show()
-		_trash_can.process_mode = Node.PROCESS_MODE_INHERIT
-	else:
-		_trash_can.hide()
-		_trash_can.process_mode = Node.PROCESS_MODE_DISABLED
+	if _trash_can_can_become_visible and not _trash_can.visible:
+		_set_node_active(_trash_can, true)
 
 #endregion
 
@@ -81,6 +125,7 @@ func _set_trash_can_state(active: bool) -> void:
 #region Basketball hoop
 
 @export_group("Basketball hoop")
+@export var _basketball_hoop_screen_notifier: VisibleOnScreenNotifier3D
 @export var _basketball_hoop: Node3D
 
 var _basketball_bought: bool = false
@@ -88,16 +133,7 @@ var _basketball_bought: bool = false
 
 func _on_basketball_hioop_screen_notifier_screen_exited() -> void:
 	if _basketball_bought and not _basketball_hoop.visible:
-		_set_basketball_hoop_state(true)
-
-
-func _set_basketball_hoop_state(active: bool) -> void:
-	if active:
-		_basketball_hoop.show()
-		_basketball_hoop.process_mode = Node.PROCESS_MODE_INHERIT
-	else:
-		_basketball_hoop.hide()
-		_basketball_hoop.process_mode = Node.PROCESS_MODE_DISABLED
+		_set_node_active(_basketball_hoop, true)
 
 #endregion
 
@@ -109,9 +145,10 @@ func get_save_data() -> Dictionary:
 	var overlapping_nodes: Array[Node3D] = Utility.get_overlapping_nodes(level_bounds)
 	var data: Dictionary = {
 		"buy_count": _buy_count,
+		"tunnel_state": _tunnel_state,
+		"hellevator_visible": _hellevator.visible,
 		"trash_can_visible": _trash_can.visible,
 		"basketball_hoop_visible": _basketball_hoop.visible,
-		"tunnel_state": _tunnel_state,
 		"persistent_nodes": SaveManager.save_persistent_nodes(self, overlapping_nodes),
 	}
 	return data
@@ -121,14 +158,16 @@ func load_save_data(data: Dictionary) -> void:
 	if not data.is_empty():
 		_buy_count = data.buy_count
 		
-		_set_trash_can_state(data.trash_can_visible)
-		_set_basketball_hoop_state(data.basketball_hoop_visible)
-		
 		_tunnel_state = data.tunnel_state
+		
+		_set_node_active(_hellevator, data.hellevator_visible)
+		_set_node_active(_trash_can, data.trash_can_visible)
+		_set_node_active(_basketball_hoop, data.basketball_hoop_visible)
 		
 		SaveManager.load_persistent_nodes(self, data.persistent_nodes)
 	else:
-		_set_trash_can_state(false)
-		_set_basketball_hoop_state(false)
+		_set_node_active(_hellevator, false)
+		_set_node_active(_trash_can, false)
+		_set_node_active(_basketball_hoop, false)
 
 #endregion
